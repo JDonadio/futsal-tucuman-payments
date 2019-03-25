cad007fce40448202597ec5f8f6dd10905f6fe3d
Javier Donadio <javier.donadio@globant.com>
2019-03-25 10:48:36 -0300

    apply filters

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { SharingService } from '../services/sharing.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FirebaseService } from '../services/firebase.service';
 import { MessagesService } from '../services/messages.service';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { ModalTeamPage } from '../modal-team/modal-team.page';
 import * as _ from 'lodash';
 
@@ -17,10 +17,12 @@ export class HomePage implements OnInit {
   public divisions: any;
   public months: any;
   public teams: any;
+  public filteredTeams: any;
   public editMode: boolean;
   public teamForm: FormGroup;
   public selectedDivision: string;
   public selectedMonth: string;
+  public searchText;
   private focusedTeam: string;
 
   constructor(
@@ -29,10 +31,13 @@ export class HomePage implements OnInit {
     private messagesService: MessagesService,
     private formBuilder: FormBuilder,
     private modalCtrl: ModalController,
+    private alertCtrl: AlertController,
     private zone: NgZone,
   ) {
     this.focusedTeam = null;
     this.editMode = false;
+    this.searchText = '';
+    this.filteredTeams = [];
     this.teamForm = this.formBuilder.group({
       name: ['', Validators.required],
       division: ['', Validators.required],
@@ -49,13 +54,30 @@ export class HomePage implements OnInit {
     this.sharingService.currentTeams.subscribe(teams => {
       this.zone.run(() => {
         if (!teams) return;
-        this.teams = teams;
-        this.teams.forEach(t => {
+        teams.forEach(t => {
           t.totalAmount = _.sumBy(t.payments, 'amount');
         });
+        this.teams = _.orderBy(teams, t => t.division.name);
+        this.filteredTeams = _.clone(this.teams);
         this.selectedMonth = '3';
       });
     });
+  }
+
+  searchTeam() {
+    if (this.searchText == '' || !this.searchText) {
+      this.filteredTeams = _.clone(this.teams);
+      return;
+    }
+
+    let byName = _.filter(this.teams, t => { return t.name.includes(this.searchText) });
+    let byDivision = _.filter(this.teams, t => { return t.division.name.includes(this.searchText) });
+    let byAmount = _.filter(this.teams, t => { return t.totalAmount == this.searchText });
+
+    if (!_.isEmpty(byName)) this.filteredTeams = _.clone(byName);
+    else if (!_.isEmpty(byDivision)) this.filteredTeams = _.clone(byDivision);
+    else if (!_.isEmpty(byAmount)) this.filteredTeams = _.clone(byAmount);
+    else this.filteredTeams = [];
   }
 
   changeEditMode(team?: any) {
@@ -94,5 +116,48 @@ export class HomePage implements OnInit {
       componentProps: { ctx: team }
     });
     await modal.present();
+  }
+
+  async showFilter() {
+    let opts = _.map(this.divisions, d => {
+      return {
+        name: d.name,
+        type: 'radio',
+        label: d.name.toUpperCase(),
+        value: d.key
+      }
+    });
+    opts.unshift({ type: 'radio', label: 'Borrar filtro', value: 'none' });
+    const alert = await this.alertCtrl.create({
+      header: 'Filtrar por División',
+      inputs: opts,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Ok',
+          handler: (data) => {
+            if (data == 'none') this.clearFilter();
+            else this.applyFilter(data);
+          }
+        }
+      ],
+      backdropDismiss: false,
+      keyboardClose: false
+    });
+
+    await alert.present();
+  }
+  
+  applyFilter(division) {
+    let byDivision = _.filter(this.teams, t => { return t.division.key == division });
+    if (!_.isEmpty(byDivision)) this.filteredTeams = _.clone(byDivision);
+    else this.filteredTeams = [];
+  }
+
+  clearFilter() {
+    this.filteredTeams = _.clone(this.teams);
   }
 }
